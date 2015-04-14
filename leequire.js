412e8738a331c2cdd102dd556437906b164bf006
leedow <644743991@qq.com>
2015-04-14 20:43:23 +0800

    init pro

---
+++ leequire.js
@@ -0,0 +1,14 @@
+/**
+ * leequire.js
+ * AMD加载器
+ * @authors leedow (644743991@qq.com)
+ * @website http://www.leedow.com
+ * @date    2015-04-14 20:40:39
+ * @version 1.0
+ */
+var require,define;
+(function(global){
+
+})(this)
+
+
